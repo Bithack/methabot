360e72048dfcd04f4449dba7b3cfb0dfc9e4307b
Emil Romanus <sdac@users.sourceforge.net>
2009-02-23 20:59:20 +0100

    Script for creating the install-time metha.h moved to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 
 aclocal &&
 autoconf &&
 libtoolize &&
 automake --add-missing;
+
+echo -n "generating metha.h installation header... "
+metha_h_success="no"
+pwd_backup=`pwd`
+cd src/libmetha/;
+if test -f "include/metha.h"; then
+    chmod u+w include/metha.h 
+fi
+echo "#ifndef _METHA__H_" > include/metha.h &&
+echo "#define _METHA__H_" >> include/metha.h &&
+echo "/**" >> include/metha.h &&
+echo " * metha.h - $(date)" >> include/metha.h &&
+echo " * Automatically generated for modules and clients." >> include/metha.h &&
+echo " * License: http://bithack.se/projects/methabot/license.html" >> include/metha.h &&
+echo " **/" >> include/metha.h &&
+echo "" >> include/metha.h &&
+cat include/metha.h.in >> include/metha.h &&
+cat *.h | grep -f include/functions.grep | grep -f include/filter.grep >> include/metha.h &&
+echo "#endif" >> include/metha.h &&
+metha_h_success="yes"
+cd $pwd_backup
+
+if test "x$metha_h_success" = "xyes"; then
+    echo "ok"
+else
+    echo "failed"
+fi

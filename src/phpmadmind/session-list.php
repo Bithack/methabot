f6fb8e6f43762b0f4ccc75b903fade1a57ae762f
Emil Romanus <sdac@bithack.se>
2009-05-11 19:42:37 +0200

    use &larr; instead of <-

--- src/phpmadmind/session-list.php
+++ src/phpmadmind/session-list.php
@@ -10,7 +10,7 @@ if ($info) {
       <td class="session"> <strong><a class="id" href="?p=session-info&amp;id=<?=$s->attributes()->id?>">session #<?=$s->attributes()->id?></a></strong> </td>
       <td><?=$s->latest?></td>
       <td class="state"><?=$s->state?></td>
-      <td class="url-info"><?=$s->crawler." &lt;- <span class=\"input\">".$s->input."</span>"?></td>
+      <td class="url-info"><?=$s->crawler." &larr; <span class=\"input\">".$s->input."</span>"?></td>
       <td>client: <a class="id" href="?p=client-info&amp;id=<?=$s->client?>"><?=substr($s->client, 0, 3)."...".substr($s->client, -3)?></a></td>
     </tr>
   <?php } ?>
